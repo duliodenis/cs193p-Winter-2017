104a27ca5203c7f285e44a77d0ae62b7ce8e6fa8
Dulio Denis <dulio.denis@yahoo.com>
2017-02-18 14:07:09 -0500

    Added Calculator initial buttons and label

--- assignments/Assignment_1_Calculator/Calculator/Calculator/ViewController.swift
+++ assignments/Assignment_1_Calculator/Calculator/Calculator/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
 }
 
