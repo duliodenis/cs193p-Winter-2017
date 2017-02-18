568c10a75718988dc9bf007305ccd35ccaee9fc2
Dulio Denis <dulio.denis@yahoo.com>
2017-02-17 19:41:14 -0500

    Initial Assignment I Calculator commit

---
+++ assignments/Assignment_1_Calculator/Calculator/Calculator/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by Dulio Denis on 2/17/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ assignments/Assignment_1_Calculator/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dulio Denis on 2/17/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
