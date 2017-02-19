d7c4928782ff758de5a1c0c18a3be8af6cefcd76
Dulio Denis <dulio.denis@yahoo.com>
2017-02-19 09:17:54 -0500

    Updated Calculator to match end of lecture 1

--- assignments/Assignment_1_Calculator/Calculator/Calculator/ViewController.swift
+++ assignments/Assignment_1_Calculator/Calculator/Calculator/ViewController.swift
@@ -10,19 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var display: UILabel?
+    // implicitly unwrapped optional
+    @IBOutlet weak var display: UILabel!
     
+    // implicit type
     var userIsInTheMiddleOfTyping = false
 
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
-            display!.text = digit
+            display.text = digit
             userIsInTheMiddleOfTyping = true
         }
     }
+    
+    // computed property
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
