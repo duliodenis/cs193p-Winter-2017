278df63bc2752a6040c1b08c19f99817e1301679
Dulio Denis <dulio.denis@yahoo.com>
2017-02-20 13:39:48 -0500

    Refactored model with closures to eliminate supporting global functions

--- assignments/Assignment_1_Calculator/Calculator/Calculator/CalculatorBrain.swift
+++ assignments/Assignment_1_Calculator/Calculator/Calculator/CalculatorBrain.swift
@@ -8,15 +8,6 @@
 
 import Foundation
 
-// global support functions
-func changeSign(operand: Double) -> Double {
-    return -operand
-}
-
-func multiply(op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
-
 struct CalculatorBrain {
     
     // optional on initialization = not set
@@ -31,14 +22,17 @@ struct CalculatorBrain {
         case equals
     }
     
-    // private dictionary of operations
+    // private extensible dictionary of operations with closures
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
         "√" : Operation.unary(sqrt),
         "cos" : Operation.unary(cos),
-        "±" : Operation.unary(changeSign),
-        "×" : Operation.binary(multiply),
+        "±" : Operation.unary({ -$0 }),
+        "×" : Operation.binary({ $0 * $1 }),
+        "÷" : Operation.binary({ $0 / $1 }),
+        "+" : Operation.binary({ $0 + $1 }),
+        "−" : Operation.binary({ $0 - $1 }),
         "=" : Operation.equals
     ]
     
